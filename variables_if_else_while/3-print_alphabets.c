d303a21166869916587b3187052ffe83f56ba004
RahafN1 <rahafalabdalh24@gmail.com>
2026-02-25 23:38:19 +0000

    print lowercase and uppercase alphabet using putchar

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase and uppercase
+ * main - prints the alphabet in lowercase then uppercase
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        putchar(c);
-
-    for (c = 'A'; c <= 'Z'; c++)
-        putchar(c);
-
-    putchar('\n');
-
-    return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }

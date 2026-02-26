bc32b38540edc836d397b28ccc5f0e7dbc860fba
RahafN1 <rahafalabdalh24@gmail.com>
2026-02-26 09:27:52 +0000

    print lowercase alphabet in reverse from z to a using putchar

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 /**
  * main - prints the lowercase alphabet in reverse
  *
- *  Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for(c='z';c >='a';c--)
-	    putchar(c);
-    putchar('\n');
-    return (0);
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }

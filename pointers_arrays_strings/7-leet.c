512449da4e743bc58ed70542914d7f08dcf2cda4
RahafN1 <rahafalabdalh232gmail.com>
2026-03-07 00:51:25 +0000

    Add leet function to encode string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+	return (s);
+}

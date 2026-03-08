61e03c272dc5243c49ad09d6b6740ac95af46a9e
RahafN1 <rahafalabdalh24@gmail.com>
2026-03-08 08:26:28 +0000

    add _strchr function that locates a character in a string

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -19,3 +24,5 @@ char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+
+#endif /* MAIN_H */

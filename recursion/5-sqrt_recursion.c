980fe8a23cfa4fd1d2fdbb3ea87435fedb13c84d
RahafN1 <rahafalabdalh24@gmail.com>
2026-03-09 22:22:00 +0000

    add _sqrt_recursion function that returns the natural square root

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int sqrt_helper(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ *
+ * Return: square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_helper(n, 0));
+}
+
+/**
+ * sqrt_helper - checks for the square root
+ * @n: number
+ * @i: number being tested
+ *
+ * Return: square root or -1
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
+}

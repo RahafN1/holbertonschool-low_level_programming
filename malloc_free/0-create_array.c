8a1b256b946c57d996eb212eefcd41111a29597e
RahafN1 <rahafalabdalh24@gmail.com>
2026-03-31 07:47:56 +0000

    Add create_array function

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *arr;
+    unsigned int i;
+
+    if (size == 0)
+        return (NULL);
+
+    arr = malloc(sizeof(char) * size);
+    if (arr == NULL)
+        return (NULL);
+
+    i = 0;
+    while (i < size)
+    {
+        arr[i] = c;
+        i++;
+    }
+
+    return (arr);
+}

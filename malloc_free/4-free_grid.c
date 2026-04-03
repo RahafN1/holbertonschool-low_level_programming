46e804d4146c55c9d811371635f617303113744c
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-03 17:05:34 +0000

    Add free_grid function that frees a 2D grid of integers

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}

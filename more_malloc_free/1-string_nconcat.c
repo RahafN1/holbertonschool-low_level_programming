aa7b54ccebd6fa2b862e796c9fc10d8afb4a6d49
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-04 15:35:21 +0000

    Add string_nconcat function

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -19,8 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!s2)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	for (len1 = 0; s1[len1]; len1++)
+		;
+	for (len2 = 0; s2[len2]; len2++)
+		;
 
 	if (n >= len2)
 		n = len2;

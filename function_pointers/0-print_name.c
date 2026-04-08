1f00f602379e18ac04628b644411615a32822a2a
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-08 07:47:31 +0000

    Create print_name function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <function_pointers.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name using a function pointer
@@ -7,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+    if (name != NULL && f != NULL)
+        f(name);
 }

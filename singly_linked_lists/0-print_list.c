f6e722e2c95fd0e53a346577611222c87a16aebd
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-22 07:57:41 +0000

    Fix all syntax errors in print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,33 +1,21 @@
 #include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the first node
  *
- * Return: integer length of string
+ * Return: the number of nodes
  */
-{
-int _strlen(char *s)
-	int i =0;
-
-	if(!s)
-		return(0);
-       while (*s++)
-	       i++;
-		return(i);
-}
-/**
- * print_list - prints a linked lists
- * @h: pointer to first node
- *
- * Return: size of list
- */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-	{
+
 	while (h)
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}

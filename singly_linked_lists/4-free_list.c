3d74b200792463f27e1142b6e7269756ae159ba1
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-22 09:49:10 +0000

    Add free_list function to free all nodes of a list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to head node
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+    list_t *node;
+    list_t *next_node;
+
+    if (head == NULL)
+        return;
+
+    node = head;
+
+    while (node)
+    {
+        next_node = node->next;
+        free(node->str);
+        free(node);
+        node = next_node;
+    }
+}

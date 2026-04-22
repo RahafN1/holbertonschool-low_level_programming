7e2f63e566cad3aa868b47cf4a4902a71783ef4e
RahafN1 <rahafalabdalh24@gmail.com>
2026-04-22 09:51:00 +0000

    Fix Betty style: use tabs instead of spaces in free_list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -8,19 +8,16 @@
  */
 void free_list(list_t *head)
 {
-    list_t *node;
-    list_t *next_node;
+	list_t *node, *next_node;
 
-    if (head == NULL)
-        return;
-
-    node = head;
-
-    while (node)
-    {
-        next_node = node->next;
-        free(node->str);
-        free(node);
-        node = next_node;
-    }
+	if (!head)
+		return;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
 }
